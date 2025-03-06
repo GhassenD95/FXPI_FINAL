94a70cc55c90a2c9efb37d92c9e2fd5bda1c24cb
oussamamouldi <148139223+oussamamouldi@users.noreply.github.com>
2025-03-06 03:50:55 +0100

    ready

---
+++ FXPI_FINAL/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module mains.fxpi_final {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens mains.fxpi_final to javafx.fxml;
+    exports mains.fxpi_final;
+}
